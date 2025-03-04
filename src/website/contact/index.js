6b7ee9751ae85dd9d151ca7e06fdbb515828b3d5
JonathanNoYu <jonathanyu10@gmail.com>
2025-03-04 15:40:10 -0500

    Added title to Iframe element

--- src/website/contact/index.js
+++ src/website/contact/index.js
@@ -4,7 +4,8 @@ function Contact() {
   return (
     <>
       <Row className="d-flex justify-content-center p-auto mt-5">
-        <iframe className="wd-dark-filter" 
+        <iframe title="Contact-Me"
+                className="wd-dark-filter" 
                 src="https://docs.google.com/forms/d/e/1FAIpQLSeEdkHdyisoJIp9ISxMr8RUX6bsKc3DYxoumNkvUtQ0aiClVA/viewform?embedded=true" 
                 width="640" 
                 height="849" 
